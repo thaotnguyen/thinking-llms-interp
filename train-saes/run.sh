55bf7f6c8d017b7fd5c13c485fc201b41f783fa9
Ivan Arcuschin <iarcuschin@gmail.com>
2025-07-07 12:32:22 +0000

    SAEs for Llama 8B layer 6 - n_clusters 51 to 56

--- train-saes/run.sh
+++ train-saes/run.sh
@@ -1,5 +1,5 @@
-MIN_CLUSTERS=41
-MAX_CLUSTERS=50
+MIN_CLUSTERS=51
+MAX_CLUSTERS=56
 N_EXAMPLES=100000  # all responses
 CLUSTERING_METHODS="gmm pca_gmm spherical_kmeans pca_kmeans agglomerative pca_agglomerative sae_topk"
 
