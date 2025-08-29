b5552d7d8de4eec10672d4142816f09c123b2f59
Ivan Arcuschin <iarcuschin@gmail.com>
2025-08-29 21:22:54 +0000

    Resps for QwQ-32B

--- generate-responses/run.sh
+++ generate-responses/run.sh
@@ -1,8 +1,11 @@
 # python generate_responses.py --model meta-llama/Llama-3.1-8B --save_every 1 --max_tokens 1000 --is_base_model
 # python generate_responses.py --model deepseek-ai/DeepSeek-R1-Distill-Llama-8B --save_every 1 --max_tokens 1000
 
-python generate_responses.py --model qwen/Qwen2.5-1.5B --save_every 1 --max_tokens 1000 --is_base_model
-python generate_responses.py --model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B --save_every 1 --max_tokens 1000
+# python generate_responses.py --model qwen/Qwen2.5-1.5B --save_every 1 --max_tokens 1000 --is_base_model
+# python generate_responses.py --model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B --save_every 1 --max_tokens 1000
 
-python generate_responses.py --model qwen/Qwen2.5-14B --save_every 1 --max_tokens 1000 --is_base_model
-python generate_responses.py --model deepseek-ai/DeepSeek-R1-Distill-Qwen-14B --save_every 1 --max_tokens 1000
+# python generate_responses.py --model qwen/Qwen2.5-14B --save_every 1 --max_tokens 1000 --is_base_model
+# python generate_responses.py --model deepseek-ai/DeepSeek-R1-Distill-Qwen-14B --save_every 1 --max_tokens 1000
+
+# python generate_responses.py --model qwen/Qwen2.5-32B --save_every 1 --max_tokens 1000 --is_base_model
+python generate_responses.py --model Qwen/QwQ-32B --save_every 1 --max_tokens 1000
