a0b912ab076fe64a2754a83e37797b22211e40ac
Ivan Arcuschin <iarcuschin@gmail.com>
2025-09-10 20:54:15 +0000

    Minor change

--- train-vectors/run_qwen_32b.sh
+++ train-vectors/run_qwen_32b.sh
@@ -3,7 +3,7 @@ python optimize_steering_vectors.py \
     --max_iters 50 \
     --n_training_examples 2048 \
     --n_eval_examples 512 \
-    --optim_minibatch_size 4 \
+    --optim_minibatch_size 1 \
     --layer 24 \
     --steering_vector_idx -1 \
     --lr "1e-2"
@@ -15,7 +15,7 @@ for cluster in {0..9}; do
         --max_iters 50 \
         --n_training_examples 2048 \
         --n_eval_examples 512 \
-        --optim_minibatch_size 4 \
+        --optim_minibatch_size 1 \
         --layer 24 \
         --steering_vector_idx $cluster \
         --lr "1e-2" \
