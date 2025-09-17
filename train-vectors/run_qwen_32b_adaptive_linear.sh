f3bf40e5cbf71af152f4b6b224f03c7187140394
Ivan Arcuschin <iarcuschin@gmail.com>
2025-09-17 10:00:16 +0000

    Script for training MLP-gated steering vectors for Qwen 32B

---
+++ train-vectors/run_qwen_32b_adaptive_linear.sh
@@ -0,0 +1,20 @@
+for cluster in {0..9}; do  
+    echo "Processing cluster: $cluster"
+    python optimize_steering_vectors.py \
+        --model Qwen/Qwen2.5-32B \
+        --max_iters 50 \
+        --n_training_examples 2048 \
+        --n_eval_examples 512 \
+        --optim_minibatch_size 4 \
+        --layer 12 \
+        --steering_vector_idx $cluster \
+        --lr "1e-2" \
+        --use_activation_perplexity_selection \
+        --steering_type adaptive_linear \
+        --adaptive_hidden 128
+done
+
+python visualize_vector_losses.py --model Qwen/Qwen2.5-32B --smoothing_sigma 100
+
+python evaluate_steering_vectors.py
+
