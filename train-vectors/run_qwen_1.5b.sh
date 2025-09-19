08c04e52061379bcc702aaea90d42badfabbd079
Ivan Arcuschin <iarcuschin@gmail.com>
2025-09-19 11:09:40 +0000

    Script for training Qwen 1.5B vectors

---
+++ train-vectors/run_qwen_1.5b.sh
@@ -0,0 +1,28 @@
+python optimize_steering_vectors.py \
+    --model Qwen/Qwen2.5-1.5B \
+    --max_iters 50 \
+    --n_training_examples 2048 \
+    --n_eval_examples 512 \
+    --optim_minibatch_size 1 \
+    --layer 24 \
+    --steering_vector_idx -1 \
+    --lr "1e-2"
+
+for cluster in {0..14}; do  
+    echo "Processing cluster: $cluster"
+    python optimize_steering_vectors.py \
+        --model Qwen/Qwen2.5-1.5B \
+        --max_iters 50 \
+        --n_training_examples 2048 \
+        --n_eval_examples 512 \
+        --optim_minibatch_size 1 \
+        --layer 24 \
+        --steering_vector_idx $cluster \
+        --lr "1e-2" \
+        --use_activation_perplexity_selection
+done
+
+python visualize_vector_losses.py --model Qwen/Qwen2.5-1.5B --smoothing_sigma 100 --steering_strategy linear
+
+python evaluate_steering_vectors.py --model Qwen/Qwen2.5-1.5B --steering_strategy linear
+
