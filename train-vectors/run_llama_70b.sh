68a33caa60f7468e6bcec7b4b4b7b4298d20f92d
Ivan Arcuschin <iarcuschin@gmail.com>
2025-09-21 23:54:42 +0000

    Minor change

--- train-vectors/run_llama_70b.sh
+++ train-vectors/run_llama_70b.sh
@@ -3,8 +3,8 @@ python optimize_steering_vectors.py \
     --max_iters 50 \
     --n_training_examples 2048 \
     --n_eval_examples 512 \
-    --optim_minibatch_size 4 \
-    --layer 12 \
+    --optim_minibatch_size 1 \
+    --layer 30 \
     --steering_vector_idx -1 \
     --lr "1e-2"
 
@@ -15,8 +15,8 @@ for cluster in {0..9}; do
         --max_iters 50 \
         --n_training_examples 2048 \
         --n_eval_examples 512 \
-        --optim_minibatch_size 4 \
-        --layer 12 \
+        --optim_minibatch_size 1 \
+        --layer 30 \
         --steering_vector_idx $cluster \
         --lr "1e-2" \
         --use_activation_perplexity_selection
