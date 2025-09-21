19a6da8607beacc5236258cb23d9cd07d24741b5
Ivan Arcuschin <iarcuschin@gmail.com>
2025-09-21 22:06:24 +0100

    Minor change

--- generate-responses/run_annotation.sh
+++ generate-responses/run_annotation.sh
@@ -5,3 +5,5 @@ python annotate_thinking.py --model qwen/QwQ-32B --layer 27 --n_clusters 10
 python annotate_thinking.py --model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B --layer 4 --n_clusters 15
 
 python annotate_thinking.py --model deepseek-ai/DeepSeek-R1-Distill-Qwen-14B --layer 38 --n_clusters 5
+
+python annotate_thinking.py --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --layer 27 --n_clusters 15
