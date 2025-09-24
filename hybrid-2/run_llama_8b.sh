1c77823a228c291a65427162167908881252708c
Ivan Arcuschin <iarcuschin@gmail.com>
2025-09-24 18:08:13 +0000

    Script for running hybrid model for Llama 8B

---
+++ hybrid-2/run_llama_8b.sh
@@ -0,0 +1,3 @@
+python hybrid_token.py --dataset gsm8k --thinking_model deepseek-ai/DeepSeek-R1-Distill-Llama-8B --base_model Qwen/Qwen2.5-14B --steering_layer 12  --sae_layer 6 --n_clusters 15 --max_new_tokens 2000 --max_thinking_tokens 2000
+
+python hybrid_token.py --dataset math500 --thinking_model deepseek-ai/DeepSeek-R1-Distill-Llama-8B --base_model Qwen/Qwen2.5-14B --steering_layer 12  --sae_layer 6 --n_clusters 15 --max_new_tokens 2000 --max_thinking_tokens 2000
