61bc4aed46fe60cc12c87a1bbaa5c5fcdceb6ffa
Ivan Arcuschin <iarcuschin@gmail.com>
2025-09-24 10:55:00 +0000

    Minor change

--- hybrid-2/run_qwen_1.5b.sh
+++ hybrid-2/run_qwen_1.5b.sh
@@ -1,1 +1,1 @@
-python hybrid_token.py --dataset math500 --thinking_model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B --base_model Qwen/Qwen2.5-Math-1.5B --steering_layer 10  --sae_layer 4 --n_clusters 15 --max_new_tokens 2000  --max_thinking_tokens 2000
+python hybrid_token.py --dataset gsm8k --thinking_model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B --base_model Qwen/Qwen2.5-Math-1.5B --steering_layer 10  --sae_layer 4 --n_clusters 15 --max_new_tokens 2000  --max_thinking_tokens 2000
