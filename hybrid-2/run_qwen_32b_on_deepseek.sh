cd7d701698b9ce0dcc4f76923a5cb5213fcc99d7
Ivan Arcuschin <iarcuschin@gmail.com>
2025-09-26 09:36:37 +0000

    Final results qwen 32b deepseek on gsm8k

--- hybrid-2/run_qwen_32b_on_deepseek.sh
+++ hybrid-2/run_qwen_32b_on_deepseek.sh
@@ -1,1 +1,3 @@
+python hybrid_token.py --dataset gsm8k --thinking_model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --base_model Qwen/Qwen2.5-32B --steering_layer 24  --sae_layer 27 --n_clusters 15 --max_new_tokens 2000  --max_thinking_tokens 2000
+
 python hybrid_token.py --dataset math500 --thinking_model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --base_model Qwen/Qwen2.5-32B --steering_layer 24  --sae_layer 27 --n_clusters 15 --max_new_tokens 2000  --max_thinking_tokens 2000
